7c35ae2026af486d0228f1c596c56b4865a14880
Victor Cruz <victor@cruzcid.com>
2025-06-19 13:54:00 -0500

    revert to ee760668e13ff04951554c6e2324a7fb9ea23ca0

---
+++ js/config.js
@@ -0,0 +1,26 @@
+/**
+ * Configuration file for MIA website
+ * Contains sensitive information and configuration values
+ */
+
+const config = {
+    emailjs: {
+        userID: 'YOUR_USER_ID', // Replace with actual EmailJS user ID
+        serviceID: 'default_service',
+        templateID: 'template_contact_form'
+    },
+    turnstile: {
+        sitekey: '0x4AAAAAABddjw-SDSpgjBDI'
+    },
+    stripe: {
+        plenoDerecho: 'https://buy.stripe.com/9B69ASapSeBh13e81x7g401',
+        estudiante: 'https://buy.stripe.com/00w28qcy0gJp27i3Lh7g402',
+        colaborador: 'https://buy.stripe.com/7sYcN4gOg2Sz5ju2Hd7g400'
+    }
+};
+
+// Prevent modification of config object
+Object.freeze(config);
+
+// Export config
+export default config; 
